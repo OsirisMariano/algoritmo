05e459606b270c6eee4267e888aeb4dbfc1f5080
Osiris Mariano <oslimacontato@gmail.com>
2023-04-05 16:38:35 -0300

    Arquivo count.rb criado

---
+++ count.rb
@@ -0,0 +1,24 @@
+=begin
+
+Given a string, write a program to count the occurrence of 
+Lowercase characters, Uppercase characters, Special characters, and Numeric values. 
+
+Examples: 
+
+Input : #GeeKs01fOr@gEEks07
+Output : 
+Upper case letters : 5
+Lower case letters : 8
+Numbers : 4
+Special Characters : 2
+
+Input : *GeEkS4GeEkS*
+Output :
+Upper case letters : 6
+Lower case letters : 4
+Numbers : 1
+Special Characters : 2
+
+link: https://www.geeksforgeeks.org/count-uppercase-lowercase-special-character-numeric-values/
+
+=end
