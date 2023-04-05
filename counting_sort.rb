844ec095b46707bc46c8f41239fccc181bac7c53
Osiris Mariano <oslimacontato@gmail.com>
2023-04-05 16:39:50 -0300

    Arquivo counting_sort.rb criado

---
+++ counting_sort.rb
@@ -0,0 +1,10 @@
+=begin
+
+Counting sort is a sorting technique based on keys between a specific range. 
+It works by counting the number of objects having distinct key values (a kind of hashing). 
+Then do some arithmetic operations to calculate the position of each 
+  object in the output sequence. 
+
+  link: https://www.geeksforgeeks.org/counting-sort/
+
+=end
