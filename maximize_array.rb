2870763244ade49d3c5271492febf4bf6542fc8f
Osiris Mariano <oslimacontato@gmail.com>
2023-04-05 16:42:57 -0300

    Arquivo maximize_array.rb  criado

---
+++ maximize_array.rb
@@ -0,0 +1,9 @@
+=begin
+
+Given an array of size n and a number k. We must modify array K a number of times. 
+Here modify array means in each operation we can replace any array 
+element arr[i] by -arr[i]. We need to perform this operation in such a way 
+that after K operations, the sum of the array must be maximum?
+
+link:https://www.geeksforgeeks.org/maximize-array-sum-after-k-negations-using-sorting/
+=end
