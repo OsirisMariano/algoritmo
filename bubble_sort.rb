b3d529393d37fbf3a0e7fba351264d0ab375e441
Osiris Mariano <oslimacontato@gmail.com>
2023-04-05 16:37:00 -0300

    Arquivo bublle_sort.rb criado

---
+++ bubble_sort.rb
@@ -0,0 +1,11 @@
+=begin 
+
+O Bubble Sort é o algoritmo de classificação mais simples que funciona 
+trocando repetidamente os elementos adjacentes se eles estiverem na ordem errada. 
+Este algoritmo não é adequado para grandes conjuntos de dados, pois sua complexidade 
+de tempo média e de pior caso é bastante alta.
+
+link: https://www.geeksforgeeks.org/bubble-sort/
+
+=end
+
