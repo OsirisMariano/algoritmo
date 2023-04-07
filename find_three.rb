186a6ea842168103b4896dc84d0a856fd1fb9ef2
Osiris Mariano <oslimacontato@gmail.com>
2023-04-07 20:20:46 -0300

    Resolução find_three

--- find_three.rb
+++ find_three.rb
@@ -8,7 +8,47 @@ Examples :
 Input: arr[] = {10, 4, 3, 50, 23, 90}
 Output: 90, 50, 23
 
+Dada uma matriz com todos os elementos distintos, encontre os três maiores elementos.
+A complexidade de tempo esperada é O(n) e o espaço extra é O(1).
+
+Exemplos :
+
+Input: arr[] = {10, 4, 3, 50, 23, 90}
+Output: 90, 50, 23
+
 link: https://www.geeksforgeeks.org/find-the-largest-three-elements-in-an-array/
 
 
 =end
+
+module Find
+  class Three
+    attr_reader :array
+
+    def initialize(array)
+      @array = array.split(",").map(&:to_i)
+    end
+
+    def find_three
+      # Array has lass than 3 numbers
+      return "You need at least 3 numbers" if array.length < 3
+
+      first = second = third = 0
+      array.each do |i|
+        if i > first
+          third = second
+          second = first
+          first = i
+
+        elsif i > second
+          third = second
+          second = i
+
+        elsif i > third
+          third = i
+        end
+      end
+      "Three largest elements are: #{[first, second, third].join(", ")}"
+    end
+  end
+end
