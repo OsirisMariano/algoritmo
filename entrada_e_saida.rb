23b20ecee9b44d2f5298113e08da74736fd8fc2d
Osiris Mariano <oslimacontato@gmail.com>
2023-04-08 20:24:08 -0300

    Exercicio de algoritmo básico de entrada e saida em Ruby

---
+++ entrada_e_saida.rb
@@ -0,0 +1,15 @@
+#1) Saida de dados na tela --> aparecer um comando na tela
+# usuário digite seu nome
+print "Digite seu nome: "
+
+#2) Programa permite que usuário preencha o que foi pedido
+# get.chomp
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+#3) Saída final
+# para chamar um dado  #{variável}
+puts "Olá #{nome} #{sobrenome}"
+
