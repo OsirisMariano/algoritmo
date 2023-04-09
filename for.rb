2183bcfa250b1d01d2c68e810190e9123b365eac
Osiris Mariano <oslimacontato@gmail.com>
2023-04-08 22:26:17 -0300

    Criando scritp for em Ruby

---
+++ for.rb
@@ -0,0 +1,13 @@
+# Leia uma lista (array) de elementos - linguagem de programação
+# Primeiro - Criar a lista
+
+linguagens = ['Ruby', 'Go', 'C++', 'Python']
+count = 1
+# Pareça na tela (puts) essa lista
+# Laço for
+
+for linguagem in linguagens
+  puts linguagem
+  puts count
+  count = count + 1
+end
