dea1e71a195f40d5789d5c7a1d3ac018e234fe56
Osiris Mariano <oslimacontato@gmail.com>
2023-04-08 22:03:11 -0300

    Criando script em Ruby unless

---
+++ unless.rb
@@ -0,0 +1,14 @@
+=begin 
+unless é equivalente a utilizar if not, utilizar unless ou if not 
+(if !condition) é uma preferencia pessoal. 
+=end
+
+#declara minha variável 
+
+x = 10
+# a menos que x seja maior que 15
+  unless x > 15
+    puts "x é menor ou igual a 15"
+  else
+    puts "x é maior que 15"
+  end
