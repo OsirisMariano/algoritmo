9cf104cb3cf6fdeee002d848322fb0446e0b8fc8
Osiris Mariano <oslimacontato@gmail.com>
2023-04-08 22:50:01 -0300

    Criando script loop em Ruby

---
+++ loop.rb
@@ -0,0 +1,14 @@
+# Criar um contador
+# Preciso de uma variável
+count = 0
+
+# Loop de execução
+loop do 
+  puts count
+  # Condição --> SE(IF) enquanto não for igual a 150
+  if count == 150
+    break
+  end
+  # Incremento --> somar 1
+  count += 1
+end
