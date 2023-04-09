7c7b98b326e5e822cc5d9ae6247c6893e8cbf32c
Osiris Mariano <oslimacontato@gmail.com>
2023-04-08 22:32:58 -0300

    Criando script while em Ruby

---
+++ while.rb
@@ -0,0 +1,14 @@
+# Comparar um número com uma condição
+
+num = 1
+
+# Imprima todos os números, a partir de e menores que 100
+# Execute esse código ENQUANDO (WHILE), num for MENOR que 100
+
+while num < 100
+  puts num
+  # precisa pegar esse num que ele imprimiu e seguir para o próximo
+  # utiliza o incremento (somar)
+  num += 1
+  
+end
